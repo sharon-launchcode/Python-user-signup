d93cfc1dba2f9427bdf2e12a90fb24268bbabe88
austin-2017 <sharon.austin.2017@gmail.com>
2017-12-31 20:43:40 -0500

    added validate variables

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-userform = """
+signupform = """
 <!DOCTYPE html>
 <html>
     <head>
@@ -36,14 +36,20 @@ userform = """
             <input type="password" id="pw1" name="pw1" value='{pw1}' /><br />
             <label for="pw2">Confirm Password: </label>
             <input type="password" id="pw2" name="pw2" value='{pw2}' /><br />
+            <p id="pwerror" class="error">{pw_error}</p>
             <label for="email">Email (Optional): </label>
             <input type="email" id="email" name="email" value='{email}' /><br />
+            <p id="emailerror" class="error">{email_error}</p>
             <button type="submit" value="submit">Submit</button>
            
         </form>
     </body>
 </html>
 """
+@app.route('/validate-pw')
+def validate_pw ():
+    return signupform.format(pw1='', pw2='', pwerror='', email='', emailerror='')
+
 
 @app.route("/")
 def index():
