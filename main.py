9de9ec3a2cab75c465649f85b30ce9d258ee9711
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-07 20:07:41 -0500

    errors not showing

--- main.py
+++ main.py
@@ -10,34 +10,6 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-
-user_input = """
-        <form action="/hello" method="post">
-            <label for="first-name">First Name:</label>
-            <input id="first-name" type="text" name="first_name" />
-            <label for="username">User Name: </label>
-            <input type="text" id="username" name="username" value='{{username}}' /><br />
-            <p id="empty_username" class="error">{{empty_username}}</p>
-            <label for="pw1">Password: </label>
-            <input type="password" id="pw1" name="pw1" value='{pw1}' /><br />
-            <p id="empty_pw1" class="error">{{empty_pw1}}</p>
-            <p id="pw1_error" class="error">{{pw1_error}}</p>
-            <label for="pw2">Confirm Password: </label>
-            <input type="password" id="pw2" name="pw2" value='{{pw2}}' /><br />
-            <p id="pw2_error" class="error">{{pw2_error}}</p>
-            <p id="empty_pw2" class="error">{{empty_pw2}}</p>
-            <p id="pwerror" class="error">{{pw_error}}</p>
-            <label for="email">Email (Optional): </label>
-            <input type="email" id="email" name="email" value='{{email}}'/><br />
-            <p id="emailerror" class="error">{{email_error}}</p>
-            <input type="submit" />
-        </form>
-    </body>
-</html>
-
-"""
-
 @app.route("/")
 def index():
     template = jinja_env.get_template('hello_form.html')
@@ -53,9 +25,6 @@ def empty_fields():
     username = request.form['username']
     pw1 = request.form['pw1']
     pw2 = request.form['pw2']
-    empty_username = ''
-    empty_pw1 =''
-    empty_pw1 =''
     if not username:
         empty_username = "A user name is required"
     if not pw1:
@@ -65,9 +34,7 @@ def empty_fields():
     template = jinja_env.get_template('hello_form.html')
     return template.render(empty_username=empty_username, empty_pw1=empty_pw1, empty_pw2=empty_pw2)  
 
-def return_errors():
-    if not pw1_error and not pw2_error and not pw_error:
-        return form.format(str)          
+        
 
 @app.route("/hello", methods=['POST'])
 def hello():
