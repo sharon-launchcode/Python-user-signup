371aa098d78d4212a352695c24a77ab19e201d05
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-08 19:19:04 -0500

    render_template no jinja

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, redirect, request
 import cgi
 import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,15 +8,7 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template('hello_form.html')
-    return template.render()
-
-def save_input():
-    username = request.form['username']
-    email = request.form['email']
-    template = jinja_env.get_template('hello_form.html')
-    return template.render(user=username, email=email)
-
+    return render_template('hello_form.html')
 
 def empty_fields():
     username = request.form['username']
@@ -32,24 +20,18 @@ def empty_fields():
         empty_pw1 = "A password must be supplied" 
     if not pw2:
         empty_pw2 = "A password must be supplied" 
-    template = jinja_env.get_template('hello_form.html')
-    return template.render(empty_username=empty_username, empty_pw1=empty_pw1, empty_pw2=empty_pw2)  
+    return render_template('hello_form.html', empty_username=empty_username, empty_pw1=empty_pw1, empty_pw2=empty_pw2)  
 
 def pw_mismatch():
     pw1 = request.form['pw1']
     pw2 = request.form['pw2']
     if pw1 != pw2:
         pw_error = "mismatch"
-    template = jinja_env.get_template('hello_form.html')
-    return template.render(pw_error=pw_error)  
-
+    return remder_template('hello_form.html', pw_error=pw_error) 
 
 @app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form['first_name']
-    template = jinja_env.get_template('hello_greeting.html')
-    return template.render(name=first_name)
-
-
-
+    return render_template('hello_greeting.html', name=first_name)
+ 
 app.run()
