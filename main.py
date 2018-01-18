948f94cea2e37551a53945ff2d7fd3722027b078
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-18 13:26:24 -0500

    chenages for consideration

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ def validate_fields():
     #https://www.infoworld.com/article/2655121/security/password-size-does-matter.html
     #https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
     if 20 < len(pw1) < 3:
-        pw1 = ''
-        pw1_error = 'Password must contain more than 3characters long, 20 max, minimum 14 recommended'
+        pw1 = pw1
+        pw1_error = 'The first Password must contain more than 3 characters long, 20 max, minimum 14 recommended'
 
     if 20 < len(pw2) < 3:
-        pw2 = ''
-        pw2_error = 'Verification password must contain more than 3 characters long, 20 max, minimum 14 recommended'    
+        pw2 = pw2
+        pw2_error = 'The second Password must contain more than 3 characters long, 20 max, minimum 14 recommended'    
 
     if pw1 != pw2:
-        pw1 = ''
-        pw2 = ''
+        pw1 = pw1
+        pw2 = pw2
         pw_error = 'Passwords do not match'
 
     #Criteria for email are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long
@@ -59,12 +59,12 @@ def validate_fields():
     if username == "":
         username_error = 'Username must be more than 3 characters but no more than 20'
     if pw1 == "":
-        pw1_error = 'Your first password field was left empty. Set a password, no fewer than 3 and no longer than 20 characters'
+        pw1empty_error = 'Your first password field was left empty. Set a password, no fewer than 3 and no longer than 20 characters'
     if pw2 == "":
-        pw2_error = 'Your second password field was left empty. Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
+        pw2empty_error = 'Your second password field was left empty. Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
 
 
-    if not username_error and not pw1_error and not pw2_error and not email_error:
+    if not username_error and not pw1_error and not pw2_error and not pw_error and not email_error and not pw1empty_error and not pw2empty_error:
         return render_template('hello_greeting.html', username = username)
 
     else:
