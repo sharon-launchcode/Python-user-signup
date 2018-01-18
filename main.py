52871cc8b62fdb7f594b0946952cc0f80e7f0657
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-18 13:45:21 -0500

    stops advance of short passwords, but need to limit to 20 and clear

--- main.py
+++ main.py
@@ -33,17 +33,17 @@ def validate_fields():
         username = username
     #https://www.infoworld.com/article/2655121/security/password-size-does-matter.html
     #https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password
-    if 20 < len(pw1) < 3:
+    if len(pw1) < 3:
         pw1 = pw1
         pw1_error = 'Password must contain more than 3characters long, 20 max, minimum 14 recommended'
 
-    if 20 < len(pw2) < 3:
+    if len(pw2) < 3:
         pw2 = pw2
         pw2_error = 'Verification password must contain more than 3 characters long, 20 max, minimum 14 recommended'    
 
     if pw1 != pw2:
-        pw1 = ''
-        pw2 = ''
+        pw1 = pw1
+        pw2 = pw2
         pw_error = 'Passwords do not match'
 
     #Criteria for email are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long
@@ -59,16 +59,16 @@ def validate_fields():
     if username == "":
         username_error = 'Username must be more than 3 characters but no more than 20'
     if pw1 == "":
-        pw1empty_error = 'Your first password field was left empty. Set a password, no fewer than 3 and no longer than 20 characters'
+        pw1_error = 'Set a password, no fewer than 3 and no longer than 20 characters'
     if pw2 == "":
-        pw2empty_error = 'Your second password field was left empty. Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
+        pw2_error = 'Enter a password to match the one above, no fewer than 3 and no longer than 20 characters'
 
 
-    if not username_error and not pw1_error and not pw2_error and not email_error and not pw1empty_error and not pw2empty_error:
+    if not username_error and not pw1_error and not pw2_error and not email_error:
         return render_template('hello_greeting.html', username = username)
 
     else:
-        return render_template('hello_form.html', username_error=username_error, pw1_error=pw1_error, pw2empty_error=pw2empty_error, pw1empty_error=pw1empty_error, pw2_error=pw2_error, email_error=email_error,
+        return render_template('hello_form.html', username_error=username_error, pw1_error=pw1_error, pw2_error=pw2_error, email_error=email_error,
         username=username, pw1=pw1, pw2=pw2, email=email)
 
 app.run()
